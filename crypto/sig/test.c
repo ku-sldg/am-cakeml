862fbf22ba5e7fd4e0e58459fab9c2c3939c446d
Michael Neises <neisesmichael@gmail.com>
2019-08-02 15:33:14 -0500

    modify files for testing, write tests

--- crypto/sig/test.c
+++ crypto/sig/test.c
@@ -5,17 +5,27 @@ int main()
 {
     char* msg = "greetings this is a test";
     unsigned long long* sig = malloc( 64*sizeof(long long) );
-    struct key_class* priv = malloc( sizeof( struct key_class ) );
+    struct key_class priv[1];
     readKey( "/usr/share/myKeys/myPrivateKey.txt", priv );
     printf( "here\n" );
+    
     signMsgWithKey( msg, sig, priv );
     for( int i=0; i<64; i++ )
     {
         printf( "%llu\n", sig[i] );
     }
 
+    uint8_t* hash = malloc( 512*8 );
+    mySha512( msg, hash );
+
+    struct key_class pub[1];
+    readKey( "/usr/share/myKeys/myPublicKey.txt", pub );
+    int whether = sigVerify( sig, hash, pub );
+    
+    printf( "\n%s\n", whether?"yes":"no" );
+
     free( sig );
-    free( priv );
+    free( hash );
 
     return(0);
 }
