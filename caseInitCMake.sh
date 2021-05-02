21ecd7d6f264b2c278294773542c47514f4069d4
Grant Jurgensen <grant@jurgensen.dev>
2021-05-02 19:21:45 +0000

    Support build on ta5/case environment

--- caseInitCMake.sh
+++ caseInitCMake.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cc=$(which arm-linux-gnueabi-gcc)
+cakec=/usr/local/bin/cake-x64-32/cake
+basis=/usr/local/bin/cake-x64-32/basis_ffi.c
 
-cmake .. -DCMAKE_C_COMPILER=${cc} -DSTATIC_LINKING=ON -DTARGET_ARCH=armv8
+cmake .. -DCMAKE_C_COMPILER=${cc} -DCAKE=${cakec} -DSTATIC_LINKING=ON -DBASIS_FILE=${basis} -DTARGET_ARCH=armv8
