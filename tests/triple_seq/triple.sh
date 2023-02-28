73a8cf05ef4ea47d85b8806892af9e15a7d1026e
Will Thomas <30wthomas@gmail.com>
2023-02-28 08:57:32 -0600

    Adding tests folder and first test suite

---
+++ tests/triple_seq/triple.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Start the server process in the background
+(../../build/apps/demo/serverdemo ../../apps/demo/server/example_server_three.json > /dev/null 2> s3.err) &
+server3_pid=$!
+
+sleep 1
+(../../build/apps/demo/serverdemo ../../apps/demo/server/example_server_two.json > /dev/null 2> s2.err) &
+server2_pid=$!
+
+sleep 1
+(../../build/apps/demo/serverdemo ../../apps/demo/server/example_server.json > /dev/null 2> s1.err) &
+server1_pid=$!
+
+sleep 1
+# Start the client process in the background
+(../../build/apps/demo/clientdemo ../../apps/demo/server/example_client.json > /dev/null 2> client.err) &
+client_pid=$!
+
+# Kill the processes using timeout
+timeout 5s kill $server3_pid
+timeout 5s kill $server2_pid
+timeout 5s kill $server1_pid
+
+cat s1.err s2.err s3.err client.err > combined.err
+rm s1.err s2.err s3.err client.err
+
+if [ ! -s "combined.err" ]; then
+  rm combined.err
+  echo "SUCCESS"
+else
+  echo "ERROR - Check combined.err for more info"
+fi
+
