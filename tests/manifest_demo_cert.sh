114d5b32a200715d14deb7dce2394a14a1d707a7
Adam Petz <ampetz@ku.edu>
2023-08-03 11:12:29 -0500

    cleaned up test scripts comments for kim, cert, cache

--- tests/manifest_demo_cert.sh
+++ tests/manifest_demo_cert.sh
@@ -3,8 +3,6 @@
 # Common Variables
 MAN_COMP=./apps/ManifestCompiler/manComp_demo
 MAN_GEN=./apps/ManifestGenerator/manGen_demo
-#BUILT_AM=./build/COMPILED_AM
-#BUILT_CONC_MAN=./concrete_manifest.json
 DEMO_FILES=../tests/DemoFiles/Cert
 
 # Server Variables
@@ -23,30 +21,15 @@ SERVER_P1_EXE_NAME=TEST_SERVER_P1_AM_EXE
 SERVER_P2_EXE_NAME=TEST_SERVER_P2_AM_EXE
 
 # Client Variables
-#CLIENT_FORM_MAN=$DEMO_FILES/Test_FormMan.sml
 CLIENT_FORM_MAN=$DEMO_FILES/FormalManifest_P0.sml
-#CLIENT_AM_LIB=$DEMO_FILES/Test_Am_Lib.sml
 CLIENT_AM_LIB=$DEMO_FILES/Test_Am_Lib_Cert.sml
 CLIENT_CONC_MAN=$DEMO_FILES/concrete_Manifest_P0.json
 
-
-
-#CLIENT_AM_LIB=$DEMO_FILES/Test_Am_Lib_Cert.sml
-#CLIENT_AM_LIB2=$DEMO_FILES/Test_Am_Lib2.sml
 CLIENT_PRIV_KEY=$DEMO_FILES/Test_PrivKey
 
-CLIENT_ONE_EXE_NAME=TEST_CLIENT_AM_ONE_EXE
-#CLIENT_TWO_EXE_NAME=TEST_CLIENT_AM_TWO_EXE
-
-
-
+CLIENT_EXE_NAME=TEST_CLIENT_AM_ONE_EXE
 
 CLIENT_TERM_FILE=$DEMO_FILES/ClientCvmTermCert.sml
-
-#SERVER_P1_TERMS_FILE=$DEMO_FILES/ServerCvmTermsCert.sml
-#SERVER_P2_TERMS_FILE=$DEMO_FILES/ServerCvmTermsCert.sml
-
-
 
 
 if [[ "$PWD" == */am-cakeml/tests ]]; then
@@ -69,16 +52,16 @@ if [[ "$PWD" == */am-cakeml/tests ]]; then
   BUILT_SERVER_AM_P1=./build/$SERVER_P1_EXE_NAME
   BUILT_SERVER_AM_P2=./build/$SERVER_P2_EXE_NAME
 
-  BUILT_CLIENT_AM_ONE=./build/$CLIENT_ONE_EXE_NAME
-  #BUILT_CLIENT_AM_TWO=./build/$CLIENT_TWO_EXE_NAME
+  BUILT_CLIENT_AM_ONE=./build/$CLIENT_EXE_NAME
 
 
-
-
-  # First let us compile the server and then run it
+  # Setup tmux windows
   tmux new-session -d -s ServerProcess 'bash -i'
 
   tmux split-window -v 'bash -i'
+  tmux split-window -v 'bash -i'
+  tmux split-window -h 'bash -i'
+  tmux select-layout even-horizontal
 
   # Start the P0 server
   tmux send-keys -t 0 "( $BUILT_SERVER_AM_P0 -m $SERVER_P0_CONC_MAN -k $SERVER_PRIV_KEY )" Enter
@@ -86,25 +69,15 @@ if [[ "$PWD" == */am-cakeml/tests ]]; then
   # Start the P1 server
   tmux send-keys -t 1 "( $BUILT_SERVER_AM_P1 -m $SERVER_P1_CONC_MAN -k $SERVER_PRIV_KEY )" Enter
 
-  tmux split-window -v 'bash -i'
-
   # Start the P2 server
   tmux send-keys -t 2 "( $BUILT_SERVER_AM_P2 -m $SERVER_P2_CONC_MAN -k $SERVER_PRIV_KEY )" Enter
-
-  # Setup tmux windows
-  tmux split-window -h 'bash -i'
-  tmux select-layout even-horizontal
   
-  # Now run the manifest compilations
-  # Sending a chain of first AM comp, run, second AM comp, run
+  # Now manifest compile and run the Client AM
+  # Sending a chain of first AM comp, then run AM
   tmux send-keys -t 3 \
-    "($MAN_COMP -c $CLIENT_TERM_FILE -o $CLIENT_ONE_EXE_NAME -om $CLIENT_CONC_MAN -m $CLIENT_FORM_MAN -l $CLIENT_AM_LIB) && \
+    "($MAN_COMP -c $CLIENT_TERM_FILE -o $CLIENT_EXE_NAME -om $CLIENT_CONC_MAN -m $CLIENT_FORM_MAN -l $CLIENT_AM_LIB) && \
      ($BUILT_CLIENT_AM_ONE -m $CLIENT_CONC_MAN -k $CLIENT_PRIV_KEY)" Enter
-     #($BUILT_CLIENT_AM_ONE -m $CLIENT_CONC_MAN -k $CLIENT_PRIV_KEY -cs)" Enter
-     
-    # && \
-    # ($MAN_COMP -c -o $CLIENT_TWO_EXE_NAME -m $CLIENT_FORM_MAN -l $CLIENT_AM_LIB2) && \
-    # ($BUILT_CLIENT_AM_TWO -m $BUILT_CONC_MAN -k $CLIENT_PRIV_KEY)" Enter
+
   tmux attach-session -d -t ServerProcess
 
 else
--- tests/manifest_demo_kim.sh
+++ tests/manifest_demo_kim.sh
@@ -3,30 +3,27 @@
 # Common Variables
 MAN_COMP=./apps/ManifestCompiler/manComp_demo
 MAN_GEN=./apps/ManifestGenerator/manGen_demo
-#BUILT_AM=./build/COMPILED_AM
-#BUILT_CONC_MAN=./concrete_manifest.json
 DEMO_FILES=../tests/DemoFiles/Kim
 
 # Server Variables
-SERVER_FORM_MAN=$DEMO_FILES/FormalManifest_P1.sml
+SERVER_P0_FORM_MAN=$DEMO_FILES/FormalManifest_P0.sml
+
+SERVER_P1_FORM_MAN=$DEMO_FILES/FormalManifest_P1.sml
 SERVER_AM_LIB=$DEMO_FILES/Test_Am_Lib_Kim.sml
 SERVER_PRIV_KEY=$DEMO_FILES/Test_Server_PrivKey
 
-SERVER_P0_FORM_MAN=$DEMO_FILES/FormalManifest_P0.sml
-
-SERVER_EXE_NAME=TEST_SERVER_AM_EXE
 SERVER_P0_EXE_NAME=TEST_SERVER_AM_P0_EXE
+SERVER_P1_EXE_NAME=TEST_SERVER_AM_EXE
 
-SERVER_P1_CONC_MAN=$DEMO_FILES/concrete_Manifest_P1.json
 SERVER_P0_CONC_MAN=$DEMO_FILES/concrete_Manifest_P0.json
+SERVER_P1_CONC_MAN=$DEMO_FILES/concrete_Manifest_P1.json
 
 # Client Variables
 CLIENT_FORM_MAN=$DEMO_FILES/FormalManifest_P0.sml
 CLIENT_AM_LIB=$DEMO_FILES/Test_Am_Lib_Kim.sml
-CLIENT_AM_LIB2=$DEMO_FILES/Test_Am_Lib_Kim2.sml
 CLIENT_PRIV_KEY=$DEMO_FILES/Test_PrivKey
 
-CLIENT_ONE_EXE_NAME=TEST_CLIENT_AM_ONE_EXE
+CLIENT_EXE_NAME=TEST_CLIENT_AM_ONE_EXE
 
 CLIENT_TERM_FILE=$DEMO_FILES/ClientCvmTermKim.sml
 
@@ -44,20 +41,21 @@ if [[ "$PWD" == */am-cakeml/tests ]]; then
   # First, generate the formal manifests
   $MAN_GEN -om $DEMO_FILES -t "kim"
 
-  # First we need to compile the server, before starting tmux (to prevent race condition)
-  $MAN_COMP -s -o $SERVER_EXE_NAME -om $SERVER_P1_CONC_MAN -m $SERVER_FORM_MAN -l $SERVER_AM_LIB
+  # Now compile the servers, before starting tmux (to prevent race condition)
+  $MAN_COMP -s -o $SERVER_P1_EXE_NAME -om $SERVER_P1_CONC_MAN -m $SERVER_P1_FORM_MAN -l $SERVER_AM_LIB
   $MAN_COMP -s -o $SERVER_P0_EXE_NAME -om $SERVER_P0_CONC_MAN -m $SERVER_P0_FORM_MAN -l $SERVER_AM_LIB
   
   
-  BUILT_SERVER_AM_ONE=./build/$SERVER_EXE_NAME
+  BUILT_SERVER_AM_ONE=./build/$SERVER_P1_EXE_NAME
   BUILT_SERVER_AM_P0=./build/$SERVER_P0_EXE_NAME
 
-  BUILT_CLIENT_AM_ONE=./build/$CLIENT_ONE_EXE_NAME
+  BUILT_CLIENT_AM=./build/$CLIENT_EXE_NAME
 
-  # First let us compile the server and then run it
+  # Setup tmux windows
   tmux new-session -d -s ServerProcess 'bash -i'
-
   tmux split-window -v 'bash -i'
+  tmux split-window -h 'bash -i'
+  tmux select-layout even-horizontal
 
    # Start the P0 server
   tmux send-keys -t 0 "( $BUILT_SERVER_AM_P0 -m $SERVER_P0_CONC_MAN -k $SERVER_PRIV_KEY )" Enter
@@ -65,15 +63,12 @@ if [[ "$PWD" == */am-cakeml/tests ]]; then
   # Start the P1 server
   tmux send-keys -t 1 "( $BUILT_SERVER_AM_ONE -m $SERVER_P1_CONC_MAN -k $SERVER_PRIV_KEY )" Enter
 
-  # Setup tmux windows
-  tmux split-window -h 'bash -i'
-  tmux select-layout even-horizontal
-  
-  # Now run the manifest compilations
-  # Sending a chain of first AM comp, run, second AM comp, run
+  # Now manifest compile and run the Client AM
+  # Sending a chain of first AM comp, then run AM
   tmux send-keys -t 2 \
-    "($MAN_COMP -c $CLIENT_TERM_FILE -o $CLIENT_ONE_EXE_NAME -om $CLIENT_P0_CONC_MAN -m $CLIENT_FORM_MAN -l $CLIENT_AM_LIB) && \
-     ($BUILT_CLIENT_AM_ONE -m $CLIENT_P0_CONC_MAN -k $CLIENT_PRIV_KEY)" Enter
+    "($MAN_COMP -c $CLIENT_TERM_FILE -o $CLIENT_EXE_NAME -om $CLIENT_P0_CONC_MAN -m $CLIENT_FORM_MAN -l $CLIENT_AM_LIB) && \
+     ($BUILT_CLIENT_AM -m $CLIENT_P0_CONC_MAN -k $CLIENT_PRIV_KEY)" Enter
+
   tmux attach-session -d -t ServerProcess
 
 else
