ae638d89d8db7f687d5d3c1094a4f973d8a8211f
Will Thomas <30wthomas@gmail.com>
2024-04-24 14:03:46 -0500

    Updating the FFI function

--- system/posix/sys/SysFFI.sml
+++ system/posix/sys/SysFFI.sml
@@ -1,5 +1,7 @@
 local 
     fun ffi_system x y = #(system) x y
+
+    fun ffi_popen x y = #(popen) x y
 in
     (* () -> int *)
     fun c_system (com) = 
@@ -8,9 +10,9 @@ in
         BString.toInt BString.LittleEndian (FFI.call ffi_system (BString.length bs) bs)
       end
 
-    fun system_exec (command) =
-      let val bs = BString.fromString com
+    fun system_exec (com) =
+      (let val bs = BString.fromString com
       in
-        BString.LittleEndian (FFI.call ffi_system (BString.length bs) bs)
-      end
+        FFI.call ffi_popen (BString.length bs) bs
+      end) : BString.bstring
 end
--- system/posix/sys/sys_ffi.c
+++ system/posix/sys/sys_ffi.c
@@ -16,31 +16,40 @@ void ffisystem(const uint8_t * c, const long clen, uint8_t * a, const long alen)
   }
 }
 
-// c is the input
-// a is the output (answer)
-char * ffipopen(const uint8_t *c, const long clen, uint8_t* a, const long alen) {
+
+/**
+ * This function is a wrapper around the popen function in C. 
+ * It takes a command as a string and executes it in a shell. 
+ * The output of the command is then piped into a buffer and copied into the output buffer.
+ * @param c The command to execute
+ * @param clen The length of the command
+ * @param a The buffer to write the output to
+ * @param alen The length of the output buffer
+*/
+void ffipopen(const uint8_t * c, const long clen, uint8_t * a, const long alen) {
     FILE *fp;
     char *output = (char *)malloc(MAX_BUFFER_SIZE * sizeof(char));
     if (output == NULL) {
-        perror("Failed to allocate memory");
-        exit(EXIT_FAILURE);
+      perror("Failed to allocate memory");
+      exit(EXIT_FAILURE);
     }
     
     memset(output, 0, MAX_BUFFER_SIZE);
 
-    fp = popen(executable_path, "r");
+    fp = popen((char*)c, "r");
     if (fp == NULL) {
-        perror("Failed to execute command");
-        exit(EXIT_FAILURE);
+      perror("Failed to execute command");
+      exit(EXIT_FAILURE);
     }
 
     fread(output, sizeof(char), MAX_BUFFER_SIZE, fp);
 
     if (pclose(fp) == -1) {
-        perror("Failed to close stream");
-        exit(EXIT_FAILURE);
+      perror("Failed to close stream");
+      exit(EXIT_FAILURE);
     }
 
-    return output;
+    strncpy((char*)a, output, MAX_BUFFER_SIZE);
+    free(output);
 }
 
