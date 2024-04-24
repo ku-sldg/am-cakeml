03b8ddf2bfa91f10396db16a1c6dde96f02e446e
Will Thomas <30wthomas@gmail.com>
2024-04-24 13:28:05 -0500

    Updating sys ffi with a more powerful fn (in-progress)

--- system/posix/sys/sys_ffi.c
+++ system/posix/sys/sys_ffi.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAX_BUFFER_SIZE 4096
+
 void ffisystem(const uint8_t * c, const long clen, uint8_t * a, const long alen) {
   int out = system((char*)c);
   // Cast down to a uint8_t return address
@@ -13,3 +15,32 @@ void ffisystem(const uint8_t * c, const long clen, uint8_t * a, const long alen)
     *a = 0xff;
   }
 }
+
+// c is the input
+// a is the output (answer)
+char * ffipopen(const uint8_t *c, const long clen, uint8_t* a, const long alen) {
+    FILE *fp;
+    char *output = (char *)malloc(MAX_BUFFER_SIZE * sizeof(char));
+    if (output == NULL) {
+        perror("Failed to allocate memory");
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(output, 0, MAX_BUFFER_SIZE);
+
+    fp = popen(executable_path, "r");
+    if (fp == NULL) {
+        perror("Failed to execute command");
+        exit(EXIT_FAILURE);
+    }
+
+    fread(output, sizeof(char), MAX_BUFFER_SIZE, fp);
+
+    if (pclose(fp) == -1) {
+        perror("Failed to close stream");
+        exit(EXIT_FAILURE);
+    }
+
+    return output;
+}
+
