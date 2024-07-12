e2558e1c5b7e4c9485f7da97f315eb3fac24ea41
Will Thomas <30wthomas@gmail.com>
2024-07-12 15:57:14 -0500

    Messed up the debug statement

--- tests/CI_Test.sh
+++ tests/CI_Test.sh
@@ -85,7 +85,7 @@ if [[ "$PWD" == */am-cakeml/tests ]]; then
   # Move to build folder
   mkdir -p $repoRoot/build
   cd $repoRoot/build
-  ls -al
+  ls -al ./bin
 
   # Generate the terms file
   $TERM_GEN -t $TERM_TYPE -o $TERM_FILE
