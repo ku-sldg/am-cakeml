3530a74727ac8c78abe2b53f80d8f805b410aa05
Will Thomas <30wthomas@gmail.com>
2024-07-12 15:51:00 -0500

    CI test should not be invoking cmake

--- tests/CI_Test.sh
+++ tests/CI_Test.sh
@@ -85,7 +85,6 @@ if [[ "$PWD" == */am-cakeml/tests ]]; then
   # Move to build folder
   mkdir -p $repoRoot/build
   cd $repoRoot/build
-  cmake ..
 
   # Generate the terms file
   $TERM_GEN -t $TERM_TYPE -o $TERM_FILE
